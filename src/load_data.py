0acdc7a054d660f4738112666576588c3ad829d2
Anastasia Mistreanu <anastasiateodora04@gmail.com>
2026-08-10 14:48:48 +0300

    Combine features and target variables, write data to SQLite database

--- src/load_data.py
+++ src/load_data.py
@@ -1,4 +1,6 @@
 from ucimlrepo import fetch_ucirepo
+import pandas as pd 
+import sqlite3
 
 dataset = fetch_ucirepo(id=863)
 X = dataset.data.features #input variables
@@ -9,3 +11,11 @@ print(X.shape) #print the shape of the input variables
 
 
 print(y.value_counts()) #print the number of samples for each risk level
+
+df = pd.concat([X,y], axis=1)  #combine input variables and target variable into a single df
+
+#connect to sqlite database
+conn = sqlite3.connect("data/maternal_health_risk.db")
+df.to_sql("maternal_health_risk", conn, if_exists="replace", index=False) #save the df to the database
+conn.close()
+
